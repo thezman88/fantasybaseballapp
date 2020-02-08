efdd7b569893cc359c156b2e2b6ad4c616df7ca4
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-02-08 07:05:38 -0500

    db connected

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+
+  env: {
+    MONGO_SRV: "mongodb+srv://ReactAdmin:admin123@reactreserve-ecqpn.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "fasdfnzxknbmxosdiu",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dlqfc22ma/image/upload",
+    STRIPE_SECRET_KEY: "sk_test_x3f3Jid854qBVfAIb0AUTyr900s8NJtefG"
+  }
+};
